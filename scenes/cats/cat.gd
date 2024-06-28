46504e98e75fc5618156e81a545c4c5f93f69505
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2024-06-28 23:19:14 +0100

    ADD: Cat/Yarn movement.
    Allows the player to charge up a shot to move the yarn, followed by the cat. Currently then resets to allow player to move again.
    
    Yarn will be replaced by yarn scene

--- scenes/cats/cat.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-unc _process(delta):
-	pass
--- scenes/main.gd
+++ scenes/main.gd
@@ -8,4 +8,3 @@ func _process(_delta):
 func _draw():
 	if player.aiming and player.pivot_position.distance_to(player.cursor_position) >= 24:
 		draw_line(player.pivot_position + player.pivot_position.direction_to(player.cursor_position) * 24, player.cursor_position, Color.SKY_BLUE, 2)
-		draw_line(player.pivot_position, player.target_position, Color.CRIMSON, 2)
