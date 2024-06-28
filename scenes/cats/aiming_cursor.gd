18ca8ad1f758aa07945322163839c22ada88ce04
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2024-06-28 22:02:11 +0100

    ADD: Updated aiming
    Simplified the cat scene into a player scene, with simpler construction and scripts

--- scenes/cats/aiming_cursor.gd
+++ scenes/cats/aiming_cursor.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 @export var aiming_line_thickness: int = 2
-@export var cat_to_line_radius: int = 10
+@export var cat_to_pivot_inner_radius: int = 10
+@export var cat_to_pivot_outer_radius: int = 60
+@export var pivot_to_cat_radius: int = 70
+
 
 func _process(_delta):
 	position = get_global_mouse_position()
